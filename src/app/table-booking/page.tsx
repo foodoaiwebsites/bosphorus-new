2b6d6c7259cccba85b928481ecd38a17c73088c2
lamimershed <lamimershed04@gmail.com>
2025-06-17 00:12:06 +0530

    fix(submodule): update

--- src/app/table-booking/page.tsx
+++ src/app/table-booking/page.tsx
@@ -1,15 +1,13 @@
-import Hero from "@/app/table-booking/(section)/Hero";
-import TableBooking from "@/app/table-booking/(section)/TableBooking";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-
-
+import Hero from "@/submodule/table-ordering/components/Hero";
+import TableBooking from "@/submodule/table-ordering/components/TableBooking";
 export default function HomePage() {
   return (
     <main className="relative flex h-full w-full">
       <div className="flex h-full w-full flex-col items-center justify-center bg-[#000000]">
         <Navbar position="absolute" />
-        <Hero />
+        <Hero imagePath="/images/table-booking/patrick-pahlke-bj3fW8xuZ28-unsplash.jpg" />
         <TableBooking />
         <Footer />
       </div>
